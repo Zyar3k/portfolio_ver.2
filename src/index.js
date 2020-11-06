9fbed449dd23417e886dc711628841c8a4845460
Arkadiusz Zygan <a.zygan@gmail.com>
2020-11-06 23:16:55 +0100

    Init project

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+const Root = () => <App />;
+
+ReactDOM.render(<Root />, document.getElementById("root"));
