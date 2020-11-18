6f2801d58c51f8cc639a6fe66fa3e4406c979b0e
Arkadiusz Zygan <a.zygan@gmail.com>
2020-11-18 18:18:49 +0100

    Add styles to home

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -1,22 +1,51 @@
 import Typical from "react-typical";
 import "./Homepage.scss";
 
+// import imgFace from "../../../images/img.jpg";
+
 const Homepage = () => {
   return (
     <>
       <div className='viewContent homeContent'>
-        <span className='start'>
-          <div className='firstLine'>Nazywam się Arek</div>
-          <div className='secondLine'>
-            <span>Zygan</span>
-            <span>Arek</span>{" "}
-          </div>
-          <Typical
-            steps={["PASJA", 2000, "PRACA", 2000, "SUKCES", 2000]}
-            loop={Infinity}
-            wrapper='h3'
-          />
-        </span>
+        <article className='introduction'>
+          <section className='top'>
+            <h4 className='topWrapper'>Nazywam się Zygan...</h4>
+            <div className='centralWrapper'>
+              <div className='nameWrapper'>
+                {/* <img src={imgFace} alt='' /> */}
+                <div className='name'>ARKADIUSZ</div>
+                <div className='lastName'>ZYGAN</div>
+              </div>
+              <span className='start'>
+                <Typical
+                  steps={[
+                    1500,
+                    "Pasja",
+                    1500,
+                    "Nauka",
+                    1500,
+                    "Praktyka",
+                    1500,
+                    "Praca",
+                    1500,
+                    "Sukces",
+                    1500,
+                  ]}
+                  loop={Infinity}
+                  wrapper='p'
+                />
+              </span>
+            </div>
+            <h4 className='bottomWrapper'>Frontend Developer</h4>
+          </section>
+          <section className='bottom'>
+            <p>
+              Bez ciągłego wzrostu i postępu, słowa takie jak poprawa,
+              osiągnięcia i sukces nie mają znaczenia
+            </p>
+            <span>Benjamin Franklin</span>
+          </section>
+        </article>
       </div>
     </>
   );
